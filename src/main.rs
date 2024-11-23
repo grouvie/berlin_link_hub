e6a9238127c0bcfa150777960e41da0b8cc2cd1a
grouvie <20428462+grouvie@users.noreply.github.com>
2024-11-23 21:25:54 +0100

    tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing::{subscriber::set_global_default, Level};
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    set_global_default(subscriber).expect("setting default subscriber failed");
+
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
